5111dea81ffa159b2f81b088de852ceb0fc4c5f4
Jonas Wills <jonas.wills@colorado.edu>
2025-09-22 11:32:07 -0600

    added a light trigger

--- level_components/enemy_trigger_spawner.gd
+++ level_components/enemy_trigger_spawner.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
+signal player_entered
+signal player_exited
 
 func _on_event_trigger_body_entered(body: Node2D) -> void:
 	if body is Player:
 		print ('player hit trigger!')
+		player_entered.emit()
 		%Timer.start()
 
 func _on_event_trigger_body_exited(body: Node2D) -> void:
 	if body is Player:
 		print ('player left trigger!')
+		player_exited.emit()
 		%Timer.stop()
 
 func _on_timer_timeout() -> void:
